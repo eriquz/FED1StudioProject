5f6aa41b000c5be5f7ec2e8b709c26c8e3f467f7
Enrique Garcia <EriquzGar10@outlook.com>
2021-01-19 21:19:39 -0600

    test

--- FED1Studiocproject/Properties/AssemblyInfo.cs
+++ FED1Studiocproject/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
+//Test1
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
