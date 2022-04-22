a8a15f5418cd765a0cdd5eab60876a4979d5e24a
Enrique Garcia <EriquzGar10@outlook.com>
2022-04-22 00:51:28 -0500

    Styling and Ajustments on formating and layout

---
+++ FED1StudioProject/JS/js.js
@@ -0,0 +1,20 @@
+﻿function toggleNav() {
+    let navBar = document.getElementById('navBar');
+    navBar.classList.toggle('expanded');
+
+    //Hamburger menu btn
+    let btnHamburger = document.getElementById('menu-btn');
+    if (navBar.classList.contains('expanded')) {
+        btnHamburger.innerHTML = "&times;";
+    } else {
+        btnHamburger.innerHTML = "&#9776;";
+    }
+}
+
+var today = new Date();
+var year = today.getFullYear();
+
+var el = document.getElementById('footerp');
+el.innerHTML = 'All Rights Reserved ' + year;
+
+
